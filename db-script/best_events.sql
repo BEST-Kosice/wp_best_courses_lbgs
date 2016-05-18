5fff4b62ed56782cf8c52de57ff902bce914af68
Peter Vasiľ <Peter Vasi>
2016-05-18 17:13:42 +0200

    fix_db_doplnenie stlpca pre prihlasenie do kurzu

--- db-script/best_events.sql
+++ db-script/best_events.sql
@@ -8,14 +8,15 @@ CREATE TABLE best_event (
     event_type varchar(100) not null,
     acad_compl varchar(20),
     fee varchar(10) not null,
-    app_deadline varchar(30)
+    app_deadline varchar(30),
+    login_url varchar(1000)
 );
 
-INSERT INTO best_event (event_name, place, dates, event_type, acad_compl, fee, app_deadline)
-VALUES ('', '', '', '', '','', '');
+INSERT INTO best_event (event_name, place, dates, event_type, acad_compl, fee, app_deadline, login_url)
+VALUES ('', '', '', '', '','', '', '');
 
 DELETE FROM best_event WHERE id_event = 1;
 
 UPDATE best_event SET event_name = '', place = '', dates = '',
-	event_type = '', acad_compl = '', fee = '', app_deadline = ''
+	event_type = '', acad_compl = '', fee = '', app_deadline = '', login_url = ''
     WHERE id_event = 2;
