6dae31af0527b286102e0dcb6fa32e7894b9ad70
Daniel Demečko <daniel.demecko@gmail.com>
2016-07-17 02:37:18 +0200

    Added shortcodes for dislaying

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,150 +1,167 @@
 /* jshint node:true */
-module.exports = function( grunt ){
-	'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-	grunt.initConfig({
-		// setting folder templates
-		dirs: {
-			css: 'assets/css',
-			sass: 'assets/sass',
-			scss: 'assets/scss',
-			js: 'assets/js'
-		},
+    grunt.initConfig({
+        // setting folder templates
+        dirs: {
+            css: 'assets/css',
+            sass: 'assets/sass',
+            scss: 'assets/scss',
+            js: 'assets/js'
+        },
 
-		//  Compile all  sass .scss files
-		sass: {
-			options: {
-	          sourceMap: true,
-	          sourceMapEmbed: false,
-	          sourceMapContents: true,
-	          includePaths: ['.']
-	        },
-        	default: {
-				files: [{
-				  expand: true,
-				  cwd: '<%= dirs.scss %>/',
-				  src: ['*.{scss,sass}'],
-				  dest: '<%= dirs.css %>/',
-				  ext: '.css'
-				}]
-        	}
+        //  Compile all  sass .scss files
+        sass: {
+            options: {
+                sourceMap: true,
+                sourceMapEmbed: false,
+                sourceMapContents: true,
+                includePaths: ['.']
+            },
+            default: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= dirs.scss %>/',
+                    src: ['*.{scss,sass}'],
+                    dest: '<%= dirs.css %>/',
+                    ext: '.css'
+                }]
+            }
 
-    	},
-
-
-
-		// Compile all .less files.
-		less: {
-			compile: {
-				options: {
-					// These paths are searched for @imports
-					paths: ['<%= less.css %>/']
-				},
-				files: [{
-					expand: true,
-					cwd: '<%= dirs.css %>/',
-					src: [
-						'*.less',
-						'!mixins.less'
-					],
-					dest: '<%= dirs.css %>/',
-					ext: '.css'
-				}]
-			}
-		},
-
-		// Minify all .css files.
-		cssmin: {
-			minify: {
-				expand: true,
-				cwd: '<%= dirs.css %>/',
-				src: ['*.css'],
-				dest: '<%= dirs.css %>/',
-				ext: '.css'
-			}
-		},
-
-		// Minify .js files.
-		uglify: {
-			options: {
-				preserveComments: 'some'
-			},
-			jsfiles: {
-				files: [{
-					expand: true,
-					cwd: '<%= dirs.js %>/',
-					src: [
-						'*.js',
-						'!*.min.js',
-						'!Gruntfile.js',
-					],
-					dest: '<%= dirs.js %>/',
-					ext: '.min.js'
-				}]
-			}
-		},
-
-		// Watch changes for assets
-		watch: {
-			sass: {
-				files: [
-					'<%= dirs.sass %>/*.scss',
-				],
-				tasks: ['sass', 'cssmin'],
-			},
-			js: {
-				files: [
-					'<%= dirs.js %>/*js',
-					'!<%= dirs.js %>/*.min.js'
-				],
-				tasks: ['uglify']
-			}
-		},
-
-		// generate languages pot file for translation
-		makepot: {
-		       target: {
-		           options: {
-		               cwd: '',                          // Directory of files to internationalize.
-		               domainPath: 'lang/',                   // Where to save the POT file.
-		               exclude: [],                      // List of files or directories to ignore.
-		               include: [],                      // List of files or directories to include.
-		               mainFile: '',                     // Main project file.
-		               potComments: '',                  // The copyright at the beginning of the POT file.
-		               potFilename: 'wp-best-courses-lbgs.pot',                  // Name of the POT file.
-		               potHeaders: {
-		                   poedit: true,                 // Includes common Poedit headers.
-		                   'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
-		               },                                // Headers to add to the generated POT file.
-		               processPot: null,                 // A callback function for manipulating the POT file.
-		               type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
-		               updateTimestamp: true,            // Whether the POT-Creation-Date should be updated without other changes.
-		               updatePoFiles: false              // Whether to update PO files in the same directory as the POT file.
-		           }
-		       }
-		   },
+        },
 
 
 
+        // Compile all .less files.
+        less: {
+            compile: {
+                options: {
+                    // These paths are searched for @imports
+                    paths: ['<%= less.css %>/']
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= dirs.css %>/',
+                    src: [
+                        '*.less',
+                        '!mixins.less'
+                    ],
+                    dest: '<%= dirs.css %>/',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        // Minify all .css files.
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: '<%= dirs.css %>/',
+                src: ['*.css'],
+                dest: '<%= dirs.css %>/',
+                ext: '.css'
+            }
+        },
+
+        cssnano: {
+            options: {
+                sourcemap: false,
+                mergeLonghand: true,
+				autoprefixer: {browsers: 'last 2 versions'},
+				mergeLonghand:true,
+            },
+            dist: {
+                files: {
+                    '<%= dirs.css %>/frontend.css': '<%= dirs.css %>/frontend.css'
+                }
+            }
+        },
 
 
-	});
 
-	// Load NPM tasks to be used here
-	grunt.loadNpmTasks( 'grunt-contrib-less' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-sass' );
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+        // Minify .js files.
+        uglify: {
+            options: {
+                preserveComments: 'some'
+            },
+            jsfiles: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= dirs.js %>/',
+                    src: [
+                        '*.js',
+                        '!*.min.js',
+                        '!Gruntfile.js',
+                    ],
+                    dest: '<%= dirs.js %>/',
+                    ext: '.min.js'
+                }]
+            }
+        },
+
+        // Watch changes for assets
+        watch: {
+            sass: {
+                files: [
+                    '<%= dirs.sass %>/*.scss',
+                ],
+                tasks: ['sass', 'cssmin'],
+            },
+            js: {
+                files: [
+                    '<%= dirs.js %>/*js',
+                    '!<%= dirs.js %>/*.min.js'
+                ],
+                tasks: ['uglify']
+            }
+        },
+
+        // generate languages pot file for translation
+        makepot: {
+            target: {
+                options: {
+                    cwd: '', // Directory of files to internationalize.
+                    domainPath: 'lang/', // Where to save the POT file.
+                    exclude: [], // List of files or directories to ignore.
+                    include: [], // List of files or directories to include.
+                    mainFile: '', // Main project file.
+                    potComments: '', // The copyright at the beginning of the POT file.
+                    potFilename: 'wp-best-courses-lbgs.pot', // Name of the POT file.
+                    potHeaders: {
+                        poedit: true, // Includes common Poedit headers.
+                        'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
+                    }, // Headers to add to the generated POT file.
+                    processPot: null, // A callback function for manipulating the POT file.
+                    type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
+                    updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
+                    updatePoFiles: false // Whether to update PO files in the same directory as the POT file.
+                }
+            }
+        },
 
 
-	// Register tasks
-	grunt.registerTask( 'default', [
-		'sass',
-		//'less',
-		'cssmin',
-		'uglify'
-	]);
+
+
+
+    });
+
+    // Load NPM tasks to be used here
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-wp-i18n');
+    grunt.loadNpmTasks('grunt-cssnano');
+
+
+    // Register tasks
+    grunt.registerTask('default', [
+        'sass',
+        //'less',
+        'cssmin',
+        'uglify'
+    ]);
 
 };
--- assets/js/shortcode.js
+++ assets/js/shortcode.js
@@ -1,38 +1,30 @@
 (function() {
     tinymce.create('tinymce.plugins.Wptuts', {
         init : function(ed, url) {
-            ed.addButton('dropcap', {
-                title : 'DropCap',
-                cmd : 'dropcap',
-                image : url + '/dropcap.jpg'
+            ed.addButton('events', {
+                text: '+ BEST Events',
+                icon: false,
+                cmd : 'events',
             });
 
-            ed.addButton('showrecent', {
-                title : 'Add recent posts shortcode',
-                cmd : 'showrecent',
-                image : url + '/recent.jpg'
-            });
-
-            ed.addCommand('dropcap', function() {
+            ed.addCommand('events', function() {
                 var selected_text = ed.selection.getContent();
                 var return_text = '';
-                return_text = '<span class="dropcap">' + selected_text + '</span>';
+                return_text = '[best_events]';
                 ed.execCommand('mceInsertContent', 0, return_text);
             });
 
-            ed.addCommand('showrecent', function() {
-                var number = prompt("How many posts you want to show ? "),
-                    shortcode;
-                if (number !== null) {
-                    number = parseInt(number);
-                    if (number > 0 && number <= 20) {
-                        shortcode = '[recent-post number="' + number + '"/]';
-                        ed.execCommand('mceInsertContent', 0, shortcode);
-                    }
-                    else {
-                        alert("The number value is invalid. It should be from 0 to 20.");
-                    }
-                }
+            ed.addButton('lbgs', {
+                text: '+ BEST LBGS',
+                icon: false,
+                cmd : 'lbgs',
+            });
+
+            ed.addCommand('lbgs', function() {
+                var selected_text = ed.selection.getContent();
+                var return_text = '';
+                return_text = '[best_lbgs]';
+                ed.execCommand('mceInsertContent', 0, return_text);
             });
         },
         // ... Hidden code
