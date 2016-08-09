f40f40e1a3ffac66068f24798deddf2ebee60693
Szakacs <petersz256@gmail.com>
2016-08-09 17:03:10 +0200

    Map JS improvements, popup boxes.
    Added popup boxes displaying some text and some rudimentary translation.
    TODO: continue or use <div> elements to display the text and any info.
    TODO: what to do with former or observer BEST groups (keep on map, or not).

--- assets/js/map.js
+++ assets/js/map.js
@@ -5,46 +5,86 @@
         Snap.load(base_url + "map-optimized.svg", onSVGLoaded ) ;
     }
 
-    function addMouseoverAnimation(svgElement, mouseoverStyle, mouseoutStyle, animationSpeed){
-        svgElement.node.onmouseover =
-            function(){
-                svgElement.animate(mouseoverStyle, animationSpeed);
-            };
-        svgElement.node.onmouseout =
-            function(){
-                svgElement.animate(mouseoutStyle, animationSpeed);
-            };
-    }
-
-
     function onSVGLoaded( data ){
         s.append( data );
-        s.svg_map
-        var countries = s.selectAll("#Countries > g");
+        countries_add_effects('#1973BE', "#144173", s.selectAll("#Countries > g"));
+        LBGs_add_effects("#FAA331", "#FFF0BE", s.selectAll("#LBGs > #current > path"));
+        LBGs_add_effects("#91C3EB", "#C8EBFA", s.selectAll("#LBGs > #former > path"));
+        LBGs_add_effects("#69CD28", "#BED746", s.selectAll("#LBGs > #observer > path"));
+    }
+
+    var animationSpeed = 500;
+
+    function countries_add_effects(color, secondaryColor, countries){
         for (var idx = 0; idx < countries.length; idx++){
             countries[idx].attr({fill: '#1973BE'});
-            addMouseoverAnimation(
-              countries[idx],
-              {fill: "#144173"},
-              {fill: '#1973BE'},
-              500
-            );
-        }
-        var cities = s.selectAll("#Towns > path");
-        for (var idx = 0; idx < cities.length; idx++){
-            cities[idx].attr({fill: '#faa331'});
-            var bbox = cities[idx].getBBox();
-
-            /* NOTE odstraňuj po sebe console log pre ostatných je potom nemožne odladiť svoje veci */
-            // console.log(bbox);
-            addMouseoverAnimation(
-              cities[idx],
-              {fill: "#fff0be", transform: 'translate('+(-bbox.cx*(2-1))+','+(-bbox.cy*(2-1))+') scale(2 2)'},
-              {fill: "#faa331", transform: 'translate('+(-bbox.cx*(1-1))+','+(-bbox.cy*(1-1))+') scale(1 1)'},
-              500
-            );
+            (function(country){
+                country.mouseover(function(){
+                    country.animate({fill: "#144173"}, animationSpeed);
+                });
+                country.mouseout(function(){
+                    country.animate({fill: '#1973BE'}, animationSpeed);
+                });
+            })(countries[idx]);
         }
     }
+
+    
+    function LBGs_add_effects(color, secondaryColor, LBGs){
+        var countryNames = {
+            'SE' : 'Švédsko', 'DK' : 'Dánsko', 'NO' : 'Nórsko', 'IS' : 'Island', 'FI' : 'Fínsko', 'RU' : 'Rusko', 
+            'BE' : 'Belgicko', 'NL' : 'Holandsko', 'GB' : 'Veľká Británia', 'IE' : 'Írsko', FR : 'Francúzsko', 
+            'DE' : 'Nemecko', 'AT' : 'Rakúsko', 'CH' : 'Švajčiarsko', 'LI' : 'Lichtenštajnsko', 'LU' : 'Luxembursko',
+            'PL' : 'Poľsko', 'LT' : 'Litva', 'LV' : 'Lotyšsko', 'EE' : 'Estónsko' , 'UA' : 'Ukrajina', 'BY' : 'Bielorusko',
+            'HR' : 'Chorvátsko', 'SK' : 'Slovensko', 'CZ' : 'Česká republika', 'HU' : 'Maďarsko', 'SI' : 'Slovinsko', 
+            'BA' : 'Bosna a Hercegovina', 'RS' : 'Srbsko', 'ME' : 'Čierna hora', 'MK' : 'Maccedónsko', 'AL' : 'Albánsko',
+            'RO' : 'Rumunsko', 'BG' : 'Bulharsko', 'MD' : 'Moldavsko', 'AL' : 'Albánsko', 'GR' : 'Grécko', 'TR' : 'Turecko',
+            'ES' : 'Španielsko', 'PT' : 'Portugalsko', 'IT' : 'Taliansko', 'MT' : 'Malta', 'AD' : 'Andora',
+            'CY' : 'Cyprus', 'AM' : 'Arménsko', 'AZ' : 'Azerbajdžan', 'GE' : 'Gruzínsko', 'KZ' : 'Kazachstan'
+        };
+        for (var idx = 0; idx < LBGs.length; idx++){
+            (function(lbg){
+                var bbox = lbg.getBBox();
+                var paper = lbg.paper;
+                //add a text and a rectangle to represent the box containing the text
+                var countrycode = lbg.node.id.substr(-2);
+                var text = paper.text(bbox.x + 20, bbox.y + 30, lbg.node.id.replace(" " + countrycode, ", " + countryNames[countrycode]));
+                /* 
+                 * if part of the text were to be outside the viewport, shift the text 
+                 * and therefore later the box that will contain the text to the left or up 
+                 * by its width or height.
+                 */ 
+                var textbbox = text.getBBox();
+                if ((paper.getBBox().x2 - textbbox.x) < textbbox.width){
+                    text.attr({x : ("-=" + text.getBBox().width)});
+                    textbbox = text.getBBox();
+                }
+                if ((paper.getBBox().y2 - textbbox.y) < textbbox.height){
+                    text.attr({y : ("-=" + text.getBBox().height)});
+                    textbbox = text.getBBox();
+                }
+                var rectangle = paper.rect(textbbox.x - 5, textbbox.y - 5, textbbox.width + 20, textbbox.height + 20, 3, 3);
+                lbg.attr({fill: color});
+                LBGs[LBGs.length-1].after(rectangle);
+                rectangle.after(text);
+                text.attr({fill: "#000000", display: "none"});
+                rectangle.attr({fill: "#ffffff", stroke: "#000000", strokewidth: "0.5px", display: "none"});
+                //onmouseover make the dot bigger and show the box with the text
+                lbg.mouseover(function(){
+                    lbg.animate({fill: secondaryColor, transform: 'translate('+(-bbox.cx*(2.5-1))+','+(-bbox.cy*(2.5-1))+') scale(2.5 2.5)'}, animationSpeed);
+                    rectangle.attr({display: "block"});
+                    text.attr({display: "block"});
+                });
+                //onmouseout make the dot small again and hide the box with the text
+                lbg.mouseout(function(){
+                    lbg.animate({fill: color, transform: 'translate('+(-bbox.cx*(1-1))+','+(-bbox.cy*(1-1))+') scale(1 1)'}, animationSpeed);
+                    rectangle.attr({display: "none"});
+                    text.attr({display: "none"});
+                });
+            })(LBGs[idx]);
+        }
+    }
+
 /*
 Scaling algorithm courtesy of http://stackoverflow.com/a/24179513
 
